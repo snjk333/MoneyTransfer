685c37319c5a1cc30d181cce6de2dce2830b5eb6
Kulbit Oleksandr <kulbitsanya0@gmail.com>
2025-11-18 19:07:19 +0100

    Wallet repo update

--- src/main/java/com/oleksandr/moneytransfer/repository/WalletRepository.java
+++ src/main/java/com/oleksandr/moneytransfer/repository/WalletRepository.java
@@ -1,9 +1,18 @@
 package com.oleksandr.moneytransfer.repository;
 
 import com.oleksandr.moneytransfer.entity.Wallet;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT w FROM Wallet w WHERE w.id = :id")
+    Optional<Wallet> findByIdWithLock(UUID id);
+
 }
